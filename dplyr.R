bfd07143d9d4ca36a264f63e2231a478811dad0e
RKLeonidas <lucashenriquehca@gmail.com>
2020-02-15 19:17:38 -0300

    objetos atômicos

--- dplyr.R
+++ dplyr.R
@@ -11,3 +11,25 @@ iris %>%
   select(Species,Petal.Length,Petal.Width,Sepal.Length,Sepal.Width) %>% 
   transmute(A=Petal.Length,B=Petal.Width,C=Sepal.Length,D=Sepal.Width)
 ?summarise()
+?iris
+a <- 1
+b <- 2
+c<-3
+d<-4
+positivo<-547
+true<- 213
+class(2L)
+y<-a
+class(y)
+z<-TRUE
+class(z)
+y<-"a"
+class(y)
+vetor1<-(y)
+a<-(1)
+b<-(2)
+vetor1<-(a,b)
+1
+0.10
+a<-3
+vetor2<-("x")
